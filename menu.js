60ae307ab666d277858830e57e3b40a33e8a31eb
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2017-08-04 19:59:22 +0300

    должно, но не работает

--- menu.js
+++ menu.js
@@ -33,25 +33,35 @@ Menu.prototype.render = function() {
 	};
 	return result += '</ul>';
 };
-function MenuItem(menuItem_href, menuItem_className, item) {
+function MenuItem(menuItem_href, menuItem_className, item_name, submenu) {
 	Container.call(this);
 	this.menuItem_href = menuItem_href;
 	this.menuItem_className = menuItem_className;
-	this.item = item;
+	this.item_name = item_name;
+	this.submenu = submenu;
 };
 MenuItem.prototype = Object.create(Container.prototype);
 MenuItem.prototype.render = function() {
-	return '<li class='+ this.menuItem_className +' href='+ this.menuItem_href +'>' + this.item + '</li>';
+	if(this.submenu) {
+		this.MenuSubMenu = new Menu('','', this.submenu);
+		console.log(this.MenuSubMenu);
+	}
+	return '<li class='+ this.menuItem_className +' href='+ this.menuItem_href +'>' + this.item_name + this.MenuSubMenu + '</li>';
 };
+
+let itemSub00 = new MenuItem('/people.html', 'people', 'Люди');
+let itemSub01 = new MenuItem('/history.html', 'history', 'История');
+let itemSub02 = new MenuItem('/factory.html', 'factory', 'Заводы');
+
 let item01 = new MenuItem('/', 'mainPage', 'Главная');
-let item02 = new MenuItem('/about.html', 'aboutPage', 'О компании');
+let item02 = new MenuItem('/about.html', 'aboutPage', 'О компании', { 0: itemSub00, 1: itemSub01, 2: itemSub02 });
 
 let menu = new Menu('#myMenu', 'myMenuClass', {0 : item01, 1 : item02});
 
 document.body.innerHTML = menu.render();
 
 setTimeout(function() {
-	menu.remove();
+	item02.remove();
 }, 2000)
 
 
