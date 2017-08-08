4996d5f69cbab2fe55b320baed520cfc71cb556c
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2017-08-08 17:26:08 +0300

    infitityLevel recursia

--- menu.js
+++ menu.js
@@ -14,18 +14,15 @@ Container.prototype.remove = function() {
 		return
 	}
 	this.elem = document.getElementById(this.id);
-	this.elem.parentNode.removeChild(this.elem);
+	if(this.elem) this.elem.parentNode.removeChild(this.elem);
 };
-function Menu(menu_id, menu_className, menu_items) {
+function Menu(menu_items) {
 	Container.call(this);
-	this.id = menu_id;
-	this.className = menu_className;
 	this.menu_items = menu_items;
 };
 Menu.prototype = Object.create(Container.prototype);
 Menu.prototype.render = function() {
-	let result = '';
-	result = '<ul id=' + this.id + ' class=' + this.className + '>';
+	let result = '<ul id=' + this.id + ' class=' + this.className + '>';
 	for(let key in this.menu_items) {
 		if(this.menu_items[key] instanceof MenuItem) {
 			result += this.menu_items[key].render();
@@ -36,58 +33,45 @@ Menu.prototype.render = function() {
 	};
 	return result += '</ul>';
 };
-function MenuItem(menuItem_href, menuItem_className, item_name, submenu) {
+function MenuItem(item_name) {
 	Container.call(this);
-	this.menuItem_href = menuItem_href;
-	this.menuItem_className = menuItem_className;
 	this.item_name = item_name;
 };
 MenuItem.prototype = Object.create(Container.prototype);
 MenuItem.prototype.render = function() {
 	return '<li class='+ this.menuItem_className +' href='+ this.menuItem_href +'>' + this.item_name + '</li>';
 };
+function rendorMenu(xhr) {
+	let items = {};
+	for(let key in xhr) {
+		console.log(typeof xhr[key]);
+		if(typeof xhr[key] == 'object') {
+			// let subMenu = rendorMenu(xhr[key]);
+			items[key] = new Menu( rendorMenu(xhr[key]) );
+		} else {
+			items[key] = new MenuItem( xhr[key] );
+		};
+	};
+	return items;
+};
+let options = {
+	0 : 'Каталог',
+	1 : 'Люди',
+	2 : {
+		0 : 'Персоны',
+		1 : 'CEO',
+		2 : 'Employers',
+		3 : {
+			0 : 'трудяги',
+			1 : 'тунеядцы'
+		}
+	},
+	3 : 'Носки'
+}
+var result = rendorMenu(options);
 
-// let itemSub00 = new MenuItem('/people.html', 'people', 'Люди');
-// let itemSub01 = new MenuItem('/history.html', 'history', 'История');
-// let itemSub02 = new MenuItem('/factory.html', 'factory', 'Заводы');
-
-// let item00 = new MenuItem('/', 'mainPage', 'Главная');
-// let item01 = new MenuItem('/about.html', 'aboutPage', 'О компании');
-// let item02 = new Menu('#about', 'aboutPage', { 0: itemSub00, 1: itemSub01, 2: itemSub02 });
-// let item03 = new MenuItem('/product.html', 'product', 'Продукт');
-
-// let menu = new Menu('#myMenu', 'myMenuClass', {0 : item00, 1 : item01, 2 : item02, 3 : item03});
-
-let item4_0 = new MenuItem('/oldSocks.html','oldSocks','Старые');
-let item4_1 = new MenuItem('/newSocks.html','newSocks','Новые');
-let menu3_0 = new Menu('/oldnew.html','oldnew', {0:item4_0, 1:item4_1});
-
-let item3_0 = new MenuItem('/crossSocks.html','crossSocks','Вязанные');
-let item3_1 = new MenuItem('/cotonSocks.html','cotonSocks','Трикотажные');
-let item3_2 = new MenuItem('/holeSocks.html','holeSocks','С дыркйой');
-let menu2_0 = new Menu('/socks.html','socks', {0:item3_0, 1:item3_1, 2:item3_2, 3:menu3_0});
-
-let item2_0 = new MenuItem('/Socks.html','Socks','Носки');
-let item2_1 = new MenuItem('/tie.html','tie','Галстуки');
-let item2_2 = new MenuItem('/pants.html','pants','Брюки');
-
-let item3_3 = new MenuItem('/female.html','female','Женские');
-let item3_4 = new MenuItem('/male.html','male','Мужские');
-let menu2_1 = new Menu('/pants.html','pants', {0:item3_3, 1:item3_4});
-
-let menu1_0 = new Menu('/product.html','product', {0:item2_0, 1:menu2_0, 2:item2_1, 3:item2_2, 4:menu2_1});
-
-let item1_0 = new MenuItem('/catalog.html','catalog','Каталог');
-let item1_1 = new MenuItem('/private.html','private','Личный кабинет');
-let item1_2 = new MenuItem('/basket.html','basket','Корзина');
-
-let menu = new Menu('#myMenu', 'myMenuClass', {0 : item1_0, 1 : menu1_0, 2 : item1_1, 3 : item1_2});
-
+let menu = new Menu(result);
 document.body.innerHTML = menu.render();
-
-// setTimeout(function() {
-// 	menu.remove();
-// }, 2000)
 
 
 
